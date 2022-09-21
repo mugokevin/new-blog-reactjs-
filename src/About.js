1b57b18c3e48b864b060ca2e45f448987034acf8
mugokevin <kevinmugo4@gmail.com>
2022-09-21 15:56:08 +0300

    initial commit

---
+++ src/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return(
+        <main className="About">
+            <h2>About</h2>
+            <p style={{marginTop:'2rem'}}>Blog app to learn react </p>
+        </main>
+
+    )
+}
+
+export default About
---
+++ src/Missing.js
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+
+const Missing = () => {
+    return(
+        <main className="Missing">
+            <h2>Page Not Found!</h2>
+            <p><Link to='.'>Visit our Homepage</Link></p>
+        </main>
+
+    )
+}
+
+export default Missing
